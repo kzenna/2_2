c896abe578d0270703dfcc8f43d5c75f1ac5c02a
kzenna <46782712+kzenna@users.noreply.github.com>
2019-07-02 03:54:54 +0400

    fix problems
    Signed-off-by: Kulakov Alex <kulakov.aleks@gmail.com>

--- diplom_adv/main_VKsearch.py
+++ diplom_adv/main_VKsearch.py
@@ -11,16 +11,18 @@ users.py
 
 """
 
-# print(users.dict_id_books, users.dict_id_music, users.dict_id_movies, users.dict_id_interests,
-#       users.dict_groups_users, users.dict_friends_users, sep='\n')
-
-
-def same_interests(interest):
-    return [user_id
-            for user_id, user_interest in users.dict_id_interests.items()
-            if user_interest == interest]
-
-
 if __name__ == '__main__':
-    same_interests('работа, семья, спорт, книги, интернет')
-    # same_interests('работа, спорт, книги')
+    users.__USER_ID__ = '545472010'
+    users.__DEBUG_MODE__ = True
+    users.__TOKEN__ = '2a3035afb76507e7737b7c80fb53d80d8f1ae404ba12e4418f74a9a918c49ebe2b74b3501c03c5ad3bcea'
+    print("Введите id VK\n \
+    enter - выход")
+    while 1:
+        command = input('>>> ')
+        if command == '':
+            print('bye')
+            break
+        else:
+            users.__USER_ID__ = command
+            users.same_interests('работа, семья, спорт, книги, интернет')
+            # same_interests('работа, спорт, книги')
